d29c4adc0a3271935da0813580c2ee0f3737ef03
JovanAleksic <jovan@insidemaps.com>
2019-07-02 22:37:26 +0200

    Initial Commit

---
+++ AsyncTaskQueue/AsyncTaskQueue.h
@@ -0,0 +1,19 @@
+//
+//  AsyncTaskQueue.h
+//  AsyncTaskQueue
+//
+//  Created by joca on 7/2/19.
+//  Copyright © 2019 joca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AsyncTaskQueue.
+FOUNDATION_EXPORT double AsyncTaskQueueVersionNumber;
+
+//! Project version string for AsyncTaskQueue.
+FOUNDATION_EXPORT const unsigned char AsyncTaskQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AsyncTaskQueue/PublicHeader.h>
+
+
